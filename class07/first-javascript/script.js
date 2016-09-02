e1109c1195a5454b034dba1b96ccdfd20d7ba3dc
Alihasadd <alisonvidler@gmail.com>
2016-09-01 22:12:41 -0400

    class 6 and 7

---
+++ class07/first-javascript/script.js
@@ -0,0 +1,30 @@
+alert ("Hi i'm ali!")
+
+
+document.write(10 * 10);
+document.write(100 / 10);
+document.write(50 - 17);
+document.write(50 + 17);
+
+
+document.write('Alison Vidler' );
+
+var num1 = 10;
+
+document.write(num1);
+
+console.log(num1)
+
+var num2 = 55;
+
+var num3 = 25;
+console.log("num3: " + num3)
+
+
+function doSomething(){
+	console.log('this is working');
+}
+
+// call the function
+doSomething();
+
