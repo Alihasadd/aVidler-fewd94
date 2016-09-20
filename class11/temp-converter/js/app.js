5b68e8480ad459227bfe90aa327ba43bf2aa9f35
Alihasadd <alisonvidler@gmail.com>
2016-09-20 19:00:27 -0400

    HW class 11

---
+++ class11/temp-converter/js/app.js
@@ -0,0 +1,15 @@
+
+
+parseFloat()
+
+$('#submit').click(function(){
+
+	parseInt($('#C').val())
+	alert(parseInt($('#C').val()));
+
+
+	parseInt($('#F').val())
+	alert(parseInt($('#F').val()));
+
+
+});
